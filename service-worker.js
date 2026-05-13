bf22147b7c874aded3a07e23e068941b96fbe0fa
Fernando Benavides <fbenavides81@me.com>
2026-05-13 16:21:31 -0400

    Add PWA support

---
+++ service-worker.js
@@ -0,0 +1,4 @@
+const CACHE = 'pitchday-v1';
+const FILES = ['/Pitch-day/', '/Pitch-day/index.html'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
